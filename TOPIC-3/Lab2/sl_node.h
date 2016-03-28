64da5d2594b82a34accdf0cd7b6489e0493d29df
rhapidfyre <mike@harrisonline.us>
2016-03-28 18:10:26 +0000

    Updated T3L2

--- TOPIC-3/Lab2/sl_node.h
+++ TOPIC-3/Lab2/sl_node.h
@@ -1,20 +1,25 @@
 #include <cstddef>
 using namespace std;
 
+// Put up header guards
 #ifndef SLNODE
 #define SLNODE
 
 class SLNode
 {
     public:
+        // Constructors/Destructor
         SLNode();
         SLNode(int contents);
         ~SLNode();
+        // Mutators
         void set_contents(int contents);
         void set_next_node(SLNode* next_node);
+        //Accessors
         int contents() const;
         SLNode* next_node() const;
     private:
+        // Data type = Pointer to other object
         SLNode* next_node_;
         int contents_;
 };
